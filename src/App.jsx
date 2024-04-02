f27723631f758466ef8e29fdf3e636027a40a0ad
suman31122000 <sumanbando31122000@gmail.com>
2024-04-03 01:18:21 +0530

    index file

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  let [count,setcount]=useState(20);
+  const increase=()=>{
+   if(count<20) setcount(count+1);
+  }
+  const decrease=()=>{
+    if(count>0) setcount(count-1);
+  }
+  return (
+    <div className='container'>
+    <p>counter-value {count}</p>
+    <div>
+    <button className='button' onClick={increase}>increase{count}</button>
+    <button className='button' onClick={decrease}>decrease{count}</button>
+    </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/todo.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter your to-do"
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
